ad4f5d6739b1cadf166a2f37feff5ea61b3fdeba
whettenr <whettenrmw@gmail.com>
2024-07-04 16:41:02 +0200

    update save location

--- benchmarks/MP3S/LibriSpeech/contextnet/train.py
+++ benchmarks/MP3S/LibriSpeech/contextnet/train.py
@@ -318,7 +318,7 @@ if __name__ == "__main__":
             "tr_splits": hparams["train_splits"],
             "dev_splits": hparams["dev_splits"],
             "te_splits": hparams["test_splits"],
-            "save_folder": hparams["output_folder"],
+            "save_folder": hparams["csv_location"],
             "merge_lst": hparams["train_splits"],
             "merge_name": "train.csv",
             "skip_prep": hparams["skip_prep"],
